e36c566bb480616e18f5c57730acd4fcedc6336d
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-07-18 12:17:03 +0300

    Add files via upload

---
+++ pyray2/utils/MapManager.py
@@ -0,0 +1,73 @@
+"""
+*
+*  __  __ _____ ____   ____                 _             
+* |  \/  |  ___|  _ \ / ___| __ _ _ __ ___ (_)_ __   __ _ 
+* | |\/| | |_  | | | | |  _ / _` | '_ ` _ \| | '_ \ / _` |
+* | |  | |  _| | |_| | |_| | (_| | | | | | | | | | | (_| |
+* |_|  |_|_|   |____/ \____|\__,_|_| |_| |_|_|_| |_|\__, |
+*                                                    |___/ 
+*
+* Licensed under the Apache License, Version 2.0 (the "License")
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+from .FS import FS
+import json
+
+class MapManager:
+    def getMaps():
+        map = []
+        time = 1
+        for maps in FS.getPK3Package("game").namelist():
+            if maps.startswith("MAPS/"):
+                if not FS.isDir(maps):
+                    if time == 1:
+                        time += 1
+                    else:
+                        file = FS.getPK3Package("game").open(maps)
+                        data = file.read()
+                        decodedData = str(data.decode("utf-8"))
+                        jsonData = json.loads(decodedData)
+                        map.append(jsonData)
+        return map
+
+    def getMapName(num):
+        return MapManager.getMaps()[num]['INFO']['NAME']
+
+    def getNextMap(num):
+        return MapManager.getMaps()[num]['INFO']['NEXT']
+
+    def isSecretMap(num):
+        secret = MapManager.getMaps()[num]['INFO']['SECRET'].lower()
+        if secret == "yes":
+            return True
+        else:
+            return False
+
+    def getMapWidth(num):
+        return MapManager.getMaps()[num]['DIMENSIONS']['WIDTH']
+
+    def getMapHeight(num):
+        return MapManager.getMaps()[num]['DIMENSIONS']['HEIGHT']
+
+    def getPlayerStartPosX(num):
+        return MapManager.getMaps()[num]['PLAYER']['START_POS_X']
+
+    def getPlayerStartPosY(num):
+        return MapManager.getMaps()[num]['PLAYER']['START_POS_Y']
+
+    def getPlayerStartDirX(num):
+        return MapManager.getMaps()[num]['PLAYER']['START_DIR_X']
+
+    def getPlayerStartDirY(num):
+        return MapManager.getMaps()[num]['PLAYER']['START_DIR_Y']
+
+    def getWallsMap(num):
+        return MapManager.getMaps()[num]['WALLS']
+
+    def getCeilsMap(num):
+        return MapManager.getMaps()[num]['CEILS']
+
+    def getFloorsMap(num):
+        return MapManager.getMaps()[num]['FLOORS']
